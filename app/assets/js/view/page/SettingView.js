f9af1df1425f35be4b9908e1ab27895e85170fb0
Joseph Z <gotouchaustralia@gmail.com>
2013-07-28 20:13:12 +1000

    Add the method to clear the local storage.

--- app/assets/js/view/page/SettingView.js
+++ app/assets/js/view/page/SettingView.js
@@ -11,6 +11,29 @@ define([
         render: function () {
             this.$el.html(this.template).trigger('create');
             return this;
+        },
+        events: {
+            'click #btn_setting_clear_storage': 'clearStorage'
+        },
+        clearStorage: function() {
+            if (navigator.userAgent.match(/(iPad|iPhone|Android)/)) {
+                navigator.notification.confirm(
+                    'All the tracks saved in local storage will be destroyed!',
+                    this.doConfirm,
+                    'Clear the local storage.',
+                    ['Yes','No']
+                );
+            } else {
+                alert('All the tracks saved in local storage have been destroyed!');
+                window.localStorage.clear();
+            }
+        },
+        doConfirm: function(buttonIndex) {
+            if (buttonIndex == 1) {
+                window.localStorage.clear();
+            } else {
+                return;
+            }
         }
     });
 
