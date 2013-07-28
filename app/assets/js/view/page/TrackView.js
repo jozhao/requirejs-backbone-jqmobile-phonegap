8a5b9a968bdc35aea4e5b1db0676e586903982cc
Joseph Z <gotouchaustralia@gmail.com>
2013-07-28 20:31:56 +1000

    Add the position checking.

--- app/assets/js/view/page/TrackView.js
+++ app/assets/js/view/page/TrackView.js
@@ -10,6 +10,7 @@ define([
         el: $("#content"),
         template: _.template(tpl),
         initialize: function() {
+            this.currentPosition();
            // this.listenTo(this.model, 'change', this.render);
         },
         render: function () {
@@ -24,7 +25,7 @@ define([
         startTrack: function() {
             this.input = this.$("#track_id");
             if (!this.input.val()) return;
-
+            // Start the track.
             Paths.create({id: this.guid(), title: this.input.val(), done: false});
             this.input.val('');
             $('#btn_track_start').parent().hide();
@@ -34,6 +35,23 @@ define([
             console.log('stop track');
             $('#btn_track_start').parent().show();
             $('#btn_track_stop').parent().hide();
+        },
+        currentPosition: function() {
+            var onSuccess = function(position) {
+                alert('Latitude: '          + position.coords.latitude          + '\n' +
+                    'Longitude: '         + position.coords.longitude         + '\n' +
+                    'Altitude: '          + position.coords.altitude          + '\n' +
+                    'Accuracy: '          + position.coords.accuracy          + '\n' +
+                    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                    'Heading: '           + position.coords.heading           + '\n' +
+                    'Speed: '             + position.coords.speed             + '\n' +
+                    'Timestamp: '         + position.timestamp                + '\n');
+            };
+            function onError(error) {
+                alert('code: '    + error.code    + '\n' +
+                    'message: ' + error.message + '\n');
+            }
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
         },
         S4: function() {
             return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
